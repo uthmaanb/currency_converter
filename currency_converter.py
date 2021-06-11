12a105df99e287e46e2363c3c7f6cfae6be167cd
Uthmaan <uthmaanbreda@gmail.com>
2021-06-11 09:08:15 +0200

    thabo

--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter import ttk
 import requests
 # from tkinter import messagebox
@@ -11,8 +12,15 @@ values1 = StringVar()
 values2 = StringVar()
 
 # code to add widgets and style window will go here
-root.geometry("450x400")
+root.geometry("450x550")
 root.title("Currency Converter (Uthmaan Breda)")
+root.config(bg='#00A868')
+root.resizable(0, 0)
+
+exchange = PhotoImage(file="exchangerate-removebg-preview.png")
+img = Label(root, image=exchange)
+img.place(x=10, y=0)
+img.config(bg='#00A868')
 
 information = requests.get('https://v6.exchangerate-api.com/v6/89dcd9e8cc7777ded2575ce1/latest/USD')
 information_json = information.json()
@@ -31,20 +39,22 @@ currency_cb = ttk.Combobox(root)
 currency_cb['values'] = currency
 currency_cb['state'] = 'readonly'
 currency_cb.set('Select Currency')
-currency_cb.place(x=10, y=50)
+currency_cb.place(x=10, y=280)
+
 
 currency_cb2 = ttk.Combobox(root)
 currency_cb2['values'] = currency2
 currency_cb2['state'] = 'readonly'
 currency_cb2.set('Select Currency')
-currency_cb2.place(x=253, y=50)
+currency_cb2.place(x=253, y=280)
 
-Label(root, text='Enter Amount:').place(x=65, y=100)
-ent1 = Entry(root)
-ent1.place(x=200, y=100)
+
+Label(root, text='Enter Amount:', bg='#00A868').place(x=65, y=330)
+ent1 = Entry(root, bg='white')
+ent1.place(x=200, y=330)
 ent1.focus()
-Label(root, text='Converted Amount:').place(x=65, y=150)
-Label(root, text='', textvariable=results).place(x=200, y=150)
+Label(root, text='Converted Amount:', bg='#00A868').place(x=65, y=380)
+Label(root, text='', textvariable=results, bg='#00A868').place(x=200, y=380)
 
 
 def convert(from_currency, to_currency, amount):
@@ -58,17 +68,40 @@ def convert(from_currency, to_currency, amount):
 
 
 def perform():
-    amount = float(ent1.get())
-    from_curr = currency_cb.get()
-    to_curr = currency_cb2.get()
+    try:
+        amount = float(ent1.get())
+        from_curr = currency_cb.get()
+        to_curr = currency_cb2.get()
 
-    converted_amount = convert(from_curr, to_curr, amount)
+        converted_amount = convert(from_curr, to_curr, amount)
 
-    results.set(converted_amount)
+        results.set(converted_amount)
+    except ValueError:
+        if ent1 != int and ent1 != float:
+            messagebox.showerror('Entry not valid', 'Enter numbers only')
+
+    except requests.exceptions.ConnectionError:
+        messagebox.showerror('Internet error', 'Internet Bad')
+    except KeyError:
+        messagebox.showerror('ERROR!!!!!!!!!!!!!!!', 'Select Currency')
 
 
-Button(root, text="<>").place(x=195, y=45)
-Button(root, text="convert", borderwidth=3, command=perform).place(x=170, y=200)
+# kill program
+def kill():
+    return root.destroy()
+
+
+def clear():
+    currency_cb.set('Select Currency')
+    currency_cb2.set('Select Currency')
+    ent1.delete(0, END)
+    ent1.focus()
+    results.set('')
+
+
+Button(root, text="CONVERT", borderwidth=3, bg='white', command=perform).place(x=180, y=430)
+Button(root, text="EXIT", borderwidth=3, bg='white', command=kill).place(x=281, y=480)
+Button(root, text="CLEAR", borderwidth=3, bg='white', command=clear).place(x=100, y=480)
 
 
 root.mainloop()  # continuously runs program in window
